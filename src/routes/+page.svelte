1d01df04b711c3b00c7e84bef7a11ffdb3ae5bb6
Luca <lucacerretini03@gmail.com>
2025-04-10 16:29:52 +0200

    Update style and main page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<header>
+    <h1>DungeoM</h1>
+    <input type="button" value="LOGIN">
+</header>
