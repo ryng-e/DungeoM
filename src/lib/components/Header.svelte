92aba2dd43e72d61b551189df0b5d63ae5dfd0e0
Luca <lucacerretini03@gmail.com>
2025-04-10 18:51:18 +0200

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,14 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+  
+    const navigateToLogin = () => {
+      goto('/login');
+    };
+  </script>
+
 <header>
-    <h1>DungeoM</h1>
-    <input type="button" value="LOGIN">
+    <h1>
+        <a href="/" class="invisibleLink">DungeoM</a>
+    </h1>
+    <button on:click={navigateToLogin}>LOGIN</button>
 </header>
