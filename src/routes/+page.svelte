d4ee89aeb953df2480d96e21ec1a708eb541cf3a
Luca <lucacerretini03@gmail.com>
2025-05-28 13:43:43 +0200

    Updated homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+    import { Avatar } from '@skeletonlabs/skeleton-svelte';
+    import { UserRound } from 'lucide-svelte';
+
+    let loggedIn: number = 0;
+
+    function Log() {
+        if(loggedIn == 0) {
+            loggedIn = 1;
+        } else {
+            loggedIn = 0;
+        }
+    }
+
+</script>
+
+<div class="flex h-[20vh] justify-around items-center">
+    <h1 class="h1 font-bold">DungeoM</h1>
+    <div>
+        {#if !loggedIn}
+            <button type="button" class="btn btn-lg preset-filled-primary-500" on:click={Log}>Login</button>
+        {:else}
+            <button type="button" on:click={Log}>
+                <Avatar name="Test Prova">
+                    <UserRound />
+                </Avatar>
+            </button>
+        {/if}
+    </div>
+</div>
