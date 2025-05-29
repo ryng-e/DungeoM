f3eed78dd8d837643c45befc3754745dafd6c405
Luca <lucacerretini03@gmail.com>
2025-05-29 10:51:23 +0200

    Create component header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,14 @@
+<div class="flex h-[30vh] items-center">
+    <div class="w-[20vw]"></div>
+    <div class="w-[30vw] justify-start">
+        <a href="/"><h1 class="h1 font-bold">DungeoM</h1></a>
+    </div>
+    <div class="flex w-[30vw] justify-end">
+        <!-- non loggato -->
+            <a href="/login">
+                <button type="button" class="btn btn-lg preset-filled-primary-500">Login</button>
+            </a>
+        <!-- loggato -->
+    </div>
+    <div class="w-[20vw]"></div>
+</div>
