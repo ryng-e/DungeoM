4a204c7654822c84b344d4fdc483622cf0d9b4a9
Luca <lucacerretini03@gmail.com>
2025-05-29 10:50:21 +0200

    Revert "Update home"
    This reverts commit a46437f97c8be549a4652e3597b174d2ff1c1e3f.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
 
-    import Header from "$lib/components/header.svelte";
+    import { Avatar } from '@skeletonlabs/skeleton-svelte';
+    import { UserRound } from 'lucide-svelte';
+
+    let loggedIn: number = 0;
+
+    function Log() {
+        if(loggedIn == 0) {
+            loggedIn = 1;
+        } else {
+            loggedIn = 0;
+        }
+    }
 
 </script>
 
-<Header />
+<div class="flex h-[20vh] justify-around items-center">
+    <h1 class="h1 font-bold">DungeoM</h1>
+    <div>
+        {#if !loggedIn}
+            <button type="button" class="btn btn-lg preset-filled-primary-500" on:click={Log}>Login</button>
+        {:else}
+            <button type="button" on:click={Log}>
+                <Avatar name="Test Prova">
+                    <UserRound />
+                </Avatar>
+            </button>
+        {/if}
+    </div>
+</div>
